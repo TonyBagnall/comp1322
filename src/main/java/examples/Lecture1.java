33851c64d611bb6e68ecb53d20f5ac294526cf88
Tony Bagnall <ajb@uea.ac.uk>
2025-02-12 12:16:23 +0000

    Lecture 1 maven code example

---
+++ src/main/java/examples/Lecture1.java
@@ -0,0 +1,9 @@
+package examples;
+
+public class Lecture1 {
+
+    public static void main(String[] args) {
+        System.out.printf("Topics covered: creating a Maven project, adding dependencies, adding plugins");
+    }
+
+}
