0037400d8f6f530828fc6387420a8bd853ba53bb
Tony Bagnall <ajb@uea.ac.uk>
2025-02-12 11:46:54 +0000

    LEcture 1 maven code example

---
+++ src/main/java/examples/LoggingExamples.java
@@ -0,0 +1,29 @@
+package examples;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class LoggingExamples {
+    private static final Logger logger = LogManager.getLogger(LoggingExamples.class);
+
+    public static void main(String[] args) {
+        logger.info("Application started");
+
+        int number = 5;
+        logger.debug("The initial number is: {}", number);
+
+        try {
+            int result = divide(10, 0);
+            logger.debug("Division result: {}", result);
+        } catch (Exception e) {
+            logger.error("An error occurred: ", e);
+        }
+
+        logger.info("Application finished");
+    }
+
+    public static int divide(int a, int b) {
+        logger.debug("Dividing {} by {}", a, b);
+        return a / b;  // This will throw an exception if b = 0
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module examples {
+    requires javafx.controls;
+    requires org.apache.logging.log4j;
+    exports examples;
+}
